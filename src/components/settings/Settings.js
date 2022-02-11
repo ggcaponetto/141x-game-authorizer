b19895135091df0b74cb2d7218bcfbeee0d6c251
Giuseppe Caponetto <ggcaponetto@gmail.com>
2022-02-12 00:17:22 +0100

    wip

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -9,6 +9,10 @@ import MenuItem from '@mui/material/MenuItem'
 import { AppContext, defaultAppContext } from '../../App'
 import { Storage } from "../storage/Storage";
 import { getBlockfrostFromContext } from "../util/util";
+import { IconButton } from '@mui/material'
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Alert from '@mui/material/Alert'
 const ll = loglevel.getLogger('main');
 
 if (process.env.NODE_ENV === 'production' && !window.logleveldebug) {
@@ -56,6 +60,8 @@ export function Settings() {
     let tempStorage = new Storage();
     storage.current = tempStorage;
   }, [storage])
+
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   useEffect(() => {
     initStorage();
   }, [initStorage])
@@ -208,47 +214,62 @@ export function Settings() {
       <TextField
         style={{width: "100%"}}
         label={t("settings:authorizer-password")}
+        type={isPasswordVisible ? "text" : "password"}
         value={getAuthorizerPasswordFromContext() || ""}
         onChange={(event) => {
-          let tempSettings = context.settings;
-          tempSettings.password = event.target.value;
-          if(context.dispatch){
-            storage.current.set("settings", tempSettings);
-            context.dispatch({
-              type: defaultAppContext.actions.replace,
-              payload: {
-                settings: tempSettings
-              }
-            });
+          if((event.target.value || "").length >= 16){
+            let tempSettings = context.settings;
+            tempSettings.password = event.target.value;
+            if(context.dispatch){
+              storage.current.set("settings", tempSettings);
+              context.dispatch({
+                type: defaultAppContext.actions.replace,
+                payload: {
+                  settings: tempSettings
+                }
+              });
+            }
           }
         }}
         InputProps={{endAdornment: (
-            <Button
-              onClick={()=>{
-                function generatePassword() {
-                  var length = 32,
-                    charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+%&/()=?!£",
-                    retVal = "";
-                  for (var i = 0, n = charset.length; i < length; ++i) {
-                    retVal += charset.charAt(Math.floor(Math.random() * n));
-                  }
-                  return retVal;
-                }
-                let tempSettings = context.settings;
-                tempSettings.password = generatePassword();
-                if(context.dispatch){
-                  storage.current.set("settings", tempSettings);
-                  context.dispatch({
-                    type: defaultAppContext.actions.replace,
-                    payload: {
-                      settings: tempSettings
+            <div style={{display: "flex", flexDirection: "row"}}>
+              <IconButton  color="warning" onClick={() => { setIsPasswordVisible(!isPasswordVisible) }}>
+                {isPasswordVisible ? <VisibilityOffIcon /> : <VisibilityIcon/>}
+              </IconButton>
+              <Button
+                onClick={()=>{
+                  function generatePassword() {
+                    var length = 32,
+                      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+%&/()=?!£",
+                      retVal = "";
+                    for (var i = 0, n = charset.length; i < length; ++i) {
+                      retVal += charset.charAt(Math.floor(Math.random() * n));
                     }
-                  });
-                }
-              }}
-            >{t(`settings:reset-app-id`)}</Button>
+                    return retVal;
+                  }
+                  let tempSettings = context.settings;
+                  tempSettings.password = generatePassword();
+                  if(context.dispatch){
+                    storage.current.set("settings", tempSettings);
+                    context.dispatch({
+                      type: defaultAppContext.actions.replace,
+                      payload: {
+                        settings: tempSettings
+                      }
+                    });
+                  }
+                }}
+              >{t(`settings:reset-app-id`)}</Button>
+            </div>
           )}}
       />
+      {(()=>{
+        if((getAuthorizerPasswordFromContext() || "").length <= 16){
+          return (
+            <Alert severity="warning">{t(`settings:use-a-stronger-password`)}</Alert>
+          )
+        }
+      })()}
       <span style={{marginTop: "1em"}} />
       <TextField
         style={{width: "100%"}}
